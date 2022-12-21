d9850dba6e420d2189b84c1cf8293541cd6a6426
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-21 13:10:33 +0530

    Added Method Overloading Concept

---
+++ OOPs Concept/MethodOverLoading.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Concept
+{
+    internal class MethodOverLoading
+    {
+         public static int PlusMethodInt(int x, int y)
+        {
+            return x + y;
+        }
+
+         public static double PlusMethodDouble(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
--- OOPs Concept/Program.cs
+++ OOPs Concept/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Net.NetworkInformation;
 using static OOPs_Concept.Abstraction;
+using static OOPs_Concept.MethodOverLoading;
 
 namespace OOPs_Concept
 {
@@ -10,7 +11,7 @@ namespace OOPs_Concept
         static void Main(string[] args)
         {
             Console.WriteLine("Please choose any one program from bellow option");
-            Console.WriteLine("1:Inheritance\n2:Abstraction\n3:Polmorphisum\n4:MethodOverriding\n5:Encapsulation");
+            Console.WriteLine("1:Inheritance\n2:Abstraction\n3:Polmorphisum\n4:MethodOverriding\n5:Encapsulation\n6:MethodOverLoading");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -46,6 +47,12 @@ namespace OOPs_Concept
                     Person myObj = new Person();
                     myObj.Name = "Jenny";
                     Console.WriteLine(myObj.Name);
+                    break;
+                case 6:
+                    int myNum1 = PlusMethodInt(8, 5);
+                    double myNum2 = PlusMethodDouble(4.3, 6.26);
+                    Console.WriteLine("Int: " + myNum1);
+                    Console.WriteLine("Double: " + myNum2);
                     break;
                 default:
                     Console.WriteLine("Please choose given option");
